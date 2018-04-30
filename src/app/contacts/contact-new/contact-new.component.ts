ecb03c8c96ea243ed35cf6e0cb279adae54e6588
KandavelS <sasipriya.k@gmail.com>
2018-04-30 13:40:30 -0500

    Update contact-new.component.ts

--- src/app/contacts/contact-new/contact-new.component.ts
+++ src/app/contacts/contact-new/contact-new.component.ts
@@ -4,20 +4,20 @@ import { IContact } from '../contact';
 import { Router } from '@angular/router';
 import { ContactService } from '../contacts.service';
 import { v4 as uuid } from 'uuid';
+import * as moment from 'moment';
 
 @Component({
   selector: 'pbk-contact-new',
-  templateUrl: './contact-new.component.html',
-  styleUrls: ['./contact-new.component.css']
+  templateUrl: './contact-new.component.html'
 })
 export class ContactNewComponent implements OnInit {
-
+  pageTitle: String = 'New Contact';
   contact: IContact = {
     id: uuid(),
     name: null,
     phone: null,
     email: '',
-    birthday: null,
+    birthday: moment().toDate(),
     avatar: null
   };
   constructor(private _contactService: ContactService, private _router: Router) { }
