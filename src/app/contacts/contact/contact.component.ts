c87c96ff6efdc63efd9a6848bddf718280bde0e0
KandavelS <sasipriya.k@gmail.com>
2018-04-30 13:41:34 -0500

    Update contact.component.ts

--- src/app/contacts/contact/contact.component.ts
+++ src/app/contacts/contact/contact.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { IContact } from '../contact';
+import * as moment from 'moment';
+import { DatePipe } from '@angular/common';
+import { ContactService } from '../contacts.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pbk-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  templateUrl: './contact.component.html'
 })
 export class ContactComponent implements OnInit {
 
   @Input() contact: IContact;
-
-  constructor() { }
+  @Input() pageTitle: String;
+  @Output() saveContact: EventEmitter<IContact> = new EventEmitter<IContact>();
+  constructor(private _contactService: ContactService, private _router: Router) { }
 
   ngOnInit() {
   }
 
+  saveAction(): void {
+    this.saveContact.emit(this.contact);
+  }
+
+  cancelAction(): void {
+    this._router.navigate(['contacts']);
+  }
 }
